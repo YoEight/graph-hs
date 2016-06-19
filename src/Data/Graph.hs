b1e012a2dedf0dfbefc608f7d1b0aaca33371bea
YoEight <yo.eight@gmail.com>
2016-06-19 20:20:31 +0200

    Pending `join` implementation

--- src/Data/Graph.hs
+++ src/Data/Graph.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE Rank2Types #-}
 --------------------------------------------------------------------------------
 -- |
 -- Module : Data.Graph
@@ -9,6 +10,68 @@
 -- Portability : non-portable
 --
 --------------------------------------------------------------------------------
-module Data.Graph where
+module Data.Graph
+    ( Graph
+    , VertexId
+    , vertices
+    , edges
+    , reduceByKey
+    ) where
 
 --------------------------------------------------------------------------------
+import Control.Applicative
+import Data.Foldable
+
+--------------------------------------------------------------------------------
+import Data.Machine
+import qualified Data.Map as M
+
+--------------------------------------------------------------------------------
+type VertexId = Integer
+
+--------------------------------------------------------------------------------
+data Graph m v e =
+    Graph
+    { vertices :: SourceT m (VertexId, v)
+    , edges    :: SourceT m (VertexId, VertexId, e)
+    }
+
+--------------------------------------------------------------------------------
+reduceByKey :: Monad m => (v -> v -> v) -> Graph m v e -> Graph m v e
+reduceByKey k g = Graph (vertices g ~> start) (edges g)
+  where
+    start = construct $ running M.empty
+
+    running m = do
+        (vid, v) <- await <|> finish m
+
+        let _F (Just v') = Just $ k v v'
+            _F _         = Just v
+
+        running $ M.alter _F vid m
+
+    finish m = do
+        traverse_ yield $ M.assocs m
+        stop
+
+--------------------------------------------------------------------------------
+join :: Monad m => Graph m v e -> Graph m u e -> Graph m (v, u) e
+join g j = Graph (teeT start (vertices g) (vertices j)) undefined
+  where
+    start = construct $ onLeft M.empty
+
+    onLeft m = do
+        (vid, v) <- awaits L <|> onRight m
+        onLeft $ M.insert vid (Left v) m
+
+    onRight m = do
+        (vid, u) <- awaits R <|> finish m
+        onRight $ M.adjust (\(Left v) -> Right (v, u)) vid m
+
+    finish m = do
+        for_ (M.assocs m) $ \(vid, res) ->
+          case res of
+            Right tup -> yield (vid, tup)
+            _         -> return ()
+
+        stop

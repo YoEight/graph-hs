520250ee4b2f0fe0e30e70f0a8b3082f6a8d68b9
YoEight <yo.eight@gmail.com>
2016-06-19 17:49:39 +0200

    Organisatin

---
+++ src/Data/Graph.hs
@@ -0,0 +1,14 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module : Data.Graph
+-- Copyright : (C) 2016 Yorick Laupa
+-- License : (see the file LICENSE)
+--
+-- Maintainer : Yorick Laupa <yo.eight@gmail.com>
+-- Stability : provisional
+-- Portability : non-portable
+--
+--------------------------------------------------------------------------------
+module Data.Graph where
+
+--------------------------------------------------------------------------------

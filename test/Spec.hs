e938378c40fd7901b6b3c5a03cd8ba7d5d42931e
YoEight <yo.eight@gmail.com>
2016-06-24 21:53:20 +0200

    Implement tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,63 @@
+import Data.List
+
+import Data.Conduit
+import Test.Tasty
+import Test.Tasty.Ingredients.Basic
+import Test.Tasty.HUnit
+
 import Data.Graph
 
-test_graph :: Graph IO Integer ()
-test_graph = fromList [ (x, x) | x <- [1..10]] []
+tests :: TestTree
+tests = testGroup "Graph tests"
+    [ testCase "Simple graph" testSimpleGraph
+    , testCase "Join graphs" testJoinGraph
+    , testCase "Connected Components" testConnectedComponents
+    ]
+
+testSimpleGraph :: IO ()
+testSimpleGraph = do
+
+    let xs    = [ (x, x) | x <- [1..10] ]
+        graph = fromList xs []
+
+    vs <- collectVertices graph
+    assertEqual "Vertices should be equal" xs vs
+
+testJoinGraph :: IO ()
+testJoinGraph = do
+
+    let xs       = [ (x, x) | x <- [1..3] ]
+        expected = [ (x, (x, x)) | x <- [1..3] ]
+        graph_1  = fromList xs []
+        graph_2  = fromList (reverse xs) []
+        src      = join (vertices graph_1) (vertices graph_2)
+
+    fin <- sourceToList src
+    assertEqual "Vertices should be equal" fin expected
+
+testConnectedComponents :: IO ()
+testConnectedComponents = do
+
+    let vs = [ (x, x) | x <- [1..6] ]
+        es = [ (1, 3, ())
+             , (1, 5, ())
+             , (3, 5, ())
+             , (2, 4, ())
+             , (2, 6, ())
+             , (4, 6, ())
+             ]
+        expected = sort [ (1, 1)
+                        , (3, 1)
+                        , (5, 1)
+                        , (2, 2)
+                        , (4, 2)
+                        , (6, 2)
+                        ]
+        graph = fromList vs es
+        cc    = connectedComponents graph
+
+    fin <- collectVertices cc
+    assertEqual "Vertices should be equal" fin expected
 
 main :: IO ()
-main = do
-  xs <- collectVertices test_graph
-  print xs
+main = defaultMainWithIngredients [consoleTestReporter] tests
